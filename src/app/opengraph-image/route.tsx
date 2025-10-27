83eff5dba647a9d5bf397c56fa4e1e00816c1388
Corban Mailloux <git@corb.co>
2025-10-26 23:59:45 -0400

    Fix config for OG.

--- src/app/opengraph-image/route.tsx
+++ src/app/opengraph-image/route.tsx
@@ -1,56 +1,66 @@
 import { ImageResponse } from 'next/og'
+import { NextRequest } from 'next/server'
 
 export const runtime = 'edge'
-export const alt = 'Well Suited Band'
-export const size = {
-  width: 1200,
-  height: 630,
-}
-export const contentType = 'image/png'
+export const dynamic = 'force-static'
+export const revalidate = false
 
-export default async function Image() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          background: 'black',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {/* Logo */}
-        <img
-          src={`${process.env.NEXT_PUBLIC_URL}/images/logo-white-bg.svg`}
-          alt="Well Suited Logo"
-          tw="w-64 h-64"
-        />
+export async function GET(request: NextRequest) {
+  const size = {
+    width: 1200,
+    height: 630,
+  }
+
+  try {
+    return new ImageResponse(
+      (
         <div
           style={{
-            fontSize: 60,
-            fontWeight: 'bold',
-            color: 'white',
-            marginTop: 20,
+            background: 'black',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          Well Suited
+          {/* Logo */}
+          <img
+            src="/images/logo-white-bg.svg"
+            alt="Well Suited Logo"
+            width={256}
+            height={256}
+            style={{
+              margin: '0 auto',
+            }}
+          />
+          <div
+            style={{
+              fontSize: 60,
+              fontWeight: 'bold',
+              color: 'white',
+              marginTop: 20,
+            }}
+          >
+            Well Suited
+          </div>
+          <div
+            style={{
+              fontSize: 30,
+              color: '#EDAA49', // brand-yellow color
+              marginTop: 10,
+            }}
+          >
+            Rock/Alternative/Americana Band
+          </div>
         </div>
-        <div
-          style={{
-            fontSize: 30,
-            color: '#EDAA49', // brand-yellow color
-            marginTop: 10,
-          }}
-        >
-          Rock/Alternative/Americana Band
-        </div>
-      </div>
-    ),
-    {
-      ...size,
-    }
-  )
+      ),
+      {
+        ...size,
+      }
+    )
+  } catch (error) {
+    return new Response('Failed to generate image', { status: 500 })
+  }
 }
