bb7261656750af6e3d47a372da69c514753cf980
Corban Mailloux <git@corb.co>
2026-09-21 14:12:36 -0400

    Config root fix.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  turbopack: {
+    root: path.join(__dirname),
+  },
   images: {
     unoptimized: true,
   },
